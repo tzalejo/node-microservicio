fa2c1e9920e4976afa1765ebe80246231fd18e9d
Alejandro Valenzuela <tzalejo@gmail.com>
2022-02-22 00:57:33 -0300

    agrego mensaje de error

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -10,35 +10,29 @@ router.post('/', upsert);
 router.put('/',secure('update'), upsert);
 
 // funciones internas
-function list(req, res){
+function list(req, res, next){
     Controller.list()
         .then((list)=>{
             response.sucess(req, res, list, 200);
         })
-        .catch((err)=>{
-            response.error(req, res, err.message, 500);
-        });
+        .catch(next); // el error lo gestiona el modulo de error
 }
 
-function get(req, res){
+function get(req, res, next){
     Controller.get(req.params.id)
         .then((user)=>{
             response.sucess(req, res, user, 200);
         })
-        .catch((err) => {
-            response.error(req, res, err.message, 500);
-        });
+        .catch(next);
 }
 
-function upsert(req, res){
+function upsert(req, res, next){
     Controller.upsert(req.body)
         .then((user)=>{
             console.log('create', user);
             response.sucess(req, res, user, 201);
         })
-        .catch((err)=> {
-            response.error(req, res, err.message, 500);
-        });
+        .catch(next);
 }
 
 
